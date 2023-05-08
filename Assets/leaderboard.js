baba1cbd8bb6570850b24d5e9876bba6c547251a
THEALIFHAKER1 <alifhaker1@gmail.com>
2023-05-08 16:51:45 +0800

    welp

---
+++ Assets/leaderboard.js
@@ -0,0 +1,52 @@
+// leaderboard.js
+
+function showNameInput() {
+    inpField.prop("disabled", true);
+    const modal = document.getElementById("modal");
+    const wpmSpan = document.getElementById("wpm");
+  
+    wpmSpan.textContent = latestWpm;
+    modal.style.display = "block";
+  
+    const submitBtn = document.getElementById("submit");
+    submitBtn.addEventListener("click", () => {
+      const playerName = document.getElementById("name-input").value;
+      const playerData = {
+        name: playerName,
+        wpm: latestWpm
+      };
+      let players = JSON.parse(localStorage.getItem('playerData')) || [];
+      players.push(playerData);
+      localStorage.setItem('playerData', JSON.stringify(players));
+      modal.style.display = "none";
+      startGame();
+    });
+  }
+  
+  function showLeaderboard() {
+    const players = JSON.parse(localStorage.getItem('playerData'));
+    const leaderboardList = $('#leaderboard-list');
+  
+    // Clear the current list
+    leaderboardList.empty();
+  
+    if (players) {
+      const sortedPlayers = players.sort((a, b) => b.wpm - a.wpm);
+      const topPlayers = sortedPlayers.slice(0, 10); // get only top 10 players
+  
+      topPlayers.forEach((player, index) => {
+        const listItem = $('<li>').text(`${index + 1}. ${player.name} - ${player.wpm} WPM`);
+        leaderboardList.append(listItem);
+      });
+    }
+  }
+  
+  const leaderboardBtn = $("#leaderboard-btn");
+  leaderboardBtn.on("click", showLeaderboard);
+  
+  const closeButton = $("#close-modal");
+  closeButton.on('click', function() {
+    // Hide the modal container
+    modalContainer.css('display', 'none');
+  });
+  
--- Assets/scripts.js
+++ Assets/scripts.js
@@ -5,16 +5,17 @@ const timeTag = $(".time span b");
 const mistakeTag = $(".mistake span");
 const wpmTag = $(".wpm span");
 const cpmTag = $(".cpm span");
-// Get the leaderboard button and modal container
-const leaderboardButton = document.querySelector('.navbar-button');
-const modalContainer = document.querySelector('#modal-container');
+const leaderboardButton = $('.navbar-button');
+const modalContainer = $('#modal-container');
 
 // Add an event listener to the leaderboard button
-leaderboardButton.addEventListener('click', function() {
+leaderboardButton.on('click', function() {
   // Show the modal container
-  modalContainer.style.display = 'block';
-});
+  modalContainer.css('display', 'block');
 
+  // Call the showLeaderboard function from leaderboard.js
+  showLeaderboard();
+});
 
 let latestWpm;
 let timer,
@@ -74,55 +75,9 @@ function initTyping() {
     }   
 }
 
-function showNameInput() {
-    inpField.prop("disabled", true);
-    const modal = document.getElementById("modal");
-    const wpmSpan = document.getElementById("wpm");
-
-    wpmSpan.textContent = latestWpm;
-    modal.style.display = "block";
-
-    const submitBtn = document.getElementById("submit");
-    submitBtn.addEventListener("click", () => {
-        const playerName = document.getElementById("name-input").value;
-        const playerData = {
-            name: playerName,
-            wpm: latestWpm
-        };
-        let players = JSON.parse(localStorage.getItem('playerData')) || [];
-        players.push(playerData);
-        localStorage.setItem('playerData', JSON.stringify(players));
-        modal.style.display = "none";
-        startGame();
-    });
-}
-const leaderboardBtn = document.getElementById("leaderboard-btn");
-leaderboardBtn.addEventListener("click", showLeaderboard);
-const closeButton = document.getElementById('close-modal');
-closeButton.addEventListener('click', function() {
-  // Hide the modal container
-  modalContainer.style.display = 'none';
-});
-
-function showLeaderboard() {
-    const players = JSON.parse(localStorage.getItem('playerData'));
-    const leaderboardList = document.getElementById('leaderboard-list');
-
-    // Clear the current list
-    leaderboardList.innerHTML = '';
-
-    if (players) {
-        const sortedPlayers = players.sort((a, b) => b.wpm - a.wpm);
-        sortedPlayers.forEach((player, index) => {
-            const listItem = document.createElement('li');
-            listItem.innerText = `${index + 1}. ${player.name} - ${player.wpm} WPM`;
-            leaderboardList.appendChild(listItem);
-        });
-    }
-}
-
-  
-
+loadParagraph();
+inpField.on("input", initTyping);
+startBtn.on("click", startGame);
 
 function initTimer() {
     if(timeLeft > 0) {
@@ -139,7 +94,6 @@ function initTimer() {
     }
 }
 
-
 function startGame() {
     inpField.prop("disabled", false);
     loadParagraph();
@@ -153,30 +107,3 @@ function startGame() {
     cpmTag.text(0);
     startBtn.text("Start");
 }
-
-
-
-function showLeaderboard() {
-    const players = JSON.parse(localStorage.getItem('playerData'));
-    const leaderboardList = document.getElementById('leaderboard-list');
-
-    // Clear the current list
-    leaderboardList.innerHTML = '';
-
-    if (players) {
-        const sortedPlayers = players.sort((a, b) => b.wpm - a.wpm);
-        const topPlayers = sortedPlayers.slice(0, 10); // get only top 10 players
-
-        topPlayers.forEach((player, index) => {
-            const listItem = document.createElement('li');
-            listItem.innerText = `${index + 1}. ${player.name} - ${player.wpm} WPM`;
-            leaderboardList.appendChild(listItem);
-        });
-    }
-}
-
-  
-
-loadParagraph();
-inpField.on("input", initTyping);
-startBtn.on("click", startGame);
